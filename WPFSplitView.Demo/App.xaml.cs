651ffd95ae5b4bdf777a2724ac13f83f33afef89
MrDaedra <mrdaedra@outlook.com>
2017-01-25 17:34:40 +0300

    Added demo project

---
+++ WPFSplitView.Demo/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace WPFSplitView.Demo
+{
+    public partial class App : Application
+    {
+    }
+}
---
+++ WPFSplitView.Demo/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace WPFSplitView.Demo
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
